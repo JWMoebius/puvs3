8f41fe45deb2663c4eddea0bf9a50e70a854976b
JWMoebius <moebiusmoebius.com>
2015-12-10 10:55:23 +0100

    Add hello.cpp for task 1

---
+++ hello.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char* argv[])
+{
+  int nodeID, numNodes;
+  
+  /* ??? */
+  MPI_Init(&argc, &argv);                      // ???
+  MPI_Comm_size(MPI_COMM_WORLD, &numNodes);    // ???
+  MPI_Comm_rank(MPI_COMM_WORLD, &nodeID);      // ???
+  
+  /* ??? */
+  printf("Hello world from process %d of %d\n", nodeID, numNodes);
+  
+  /* ??? */
+  MPI_Finalize();
+  
+  return 0;
+}
+
+/*
+mpic++ hello.cpp
+mpirun -mca btl_tcp_if_exclude lo -np 2 hello.exe
+*/
